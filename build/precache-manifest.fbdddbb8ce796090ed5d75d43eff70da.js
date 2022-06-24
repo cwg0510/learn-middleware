acf0d3a4fc13359db704fe2147da446d8b0797c5
cwg0510 <cwg0510@gmail.com>
2022-06-24 14:43:49 +0900

    first commit

---
+++ build/precache-manifest.fbdddbb8ce796090ed5d75d43eff70da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf05f5e0ce5ed1ae3b2b",
+    "url": "/static/css/main.87fcae19.chunk.css"
+  },
+  {
+    "revision": "bf05f5e0ce5ed1ae3b2b",
+    "url": "/static/js/main.7b676d3b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c25d1efbed88a0cba674",
+    "url": "/static/js/2.0b4bc908.chunk.js"
+  },
+  {
+    "revision": "85817804d12c2ac57bfee4be04fa7a93",
+    "url": "/index.html"
+  }
+];
---
+++ src/containers/SampleContainer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Sample from '../components/Sample';
+import { getPost, getUsers } from '../modules/sample';
+
+const { useEffect } = React;
+const SampleContainer = ({
+  getPost,
+  getUsers,
+  post,
+  users,
+  loadingPost,
+  loadingUsers
+}) => {
+  // 클래스 형태 컴포넌트였더라면, componentDidMount
+  useEffect(() => {
+       getPost(1);
+       getUsers();
+    }, []);
+  return (
+    <Sample
+      post={post}
+      users={users}
+      loadingPost={loadingPost}
+      loadingUsers={loadingUsers}
+    />
+  );
+};
+
+export default connect(
+  ({ sample, loading }) => ({
+    post: sample.post,
+    users: sample.users,
+    loadingPost: loading['sample/GET_POST'],
+    loadingUsers: loading['sample/GET_USERS']
+  }),
+  {
+    getPost,
+    getUsers
+  }
+)(SampleContainer);
+
+
+
+
+// {
+//   changeInput : (text) => {
+//     dispatch(changeInput(text));
+//   }
+
+
+// }
